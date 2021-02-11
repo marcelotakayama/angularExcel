95c3a79ee490901020fc383276bfa61075062647
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-11 16:18:51 -0300

    removendo coisas desnecessarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { SheetJSComponent } from './sheet.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SheetJSComponent ],
+  declarations: [ AppComponent, SheetJSComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
