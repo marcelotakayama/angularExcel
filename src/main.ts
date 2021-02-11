5499266b8d7efcd282abeae52d68878ec603ef54
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-11 17:16:15 -0300

    agr vai

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import './polyfills';
-
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
